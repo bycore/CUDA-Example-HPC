82dcfe042b1efeee9ffb5a8847b6cb61285d0750
boyce <boyce.core@gmail.com>
2022-09-07 20:28:49 +0800

    complete bitonic sort

--- bitonicSort.cu
+++ bitonicSort.cu
@@ -6,20 +6,98 @@
 void bitonic_sort(float* values, int size);
 __global__ void bitonic_sort_step(float* dev_values, int j, int k);
 
+__device__ void swap_float(float* f1, float* f2) {
+    float tmp = *f1;
+    *f1 = *f2;
+    *f2 = tmp;
+}
+
+__global__ void _bitonic_sort(float* d_arr, unsigned stride, unsigned inner_stride) {
+    unsigned flipper = inner_stride >> 1;
+    unsigned tid = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned tid_other = tid ^ flipper;
+
+    if (tid < tid_other) {
+        // 操纵左侧的半部分
+        if ((tid & stride) == 0) {
+            // 此处将留升序
+            if (d_arr[tid] > d_arr[tid_other]) {
+                swap_float(&d_arr[tid], &d_arr[tid_other]);
+            }
+        } else {
+            // 此处将留降序
+            if (d_arr[tid] < d_arr[tid_other]) {
+                swap_float(&d_arr[tid], &d_arr[tid_other]);
+            }
+        }
+    }
+}
+
+/// entry point for gpu float sorting
+/// \param arr memory on gpu device
+/// \param len the length of the array
+void float_sort(float arr[], int len) {
+    // 首先检查长度是否为 2 的幂
+    unsigned twoUpper = 1;
+    for (; twoUpper < len; twoUpper <<= 1) {
+        if (twoUpper == len) {
+            break;
+        }
+    }
+    // 如果是 host 指针，返回
+    cudaPointerAttributes attrs;
+    cudaPointerGetAttributes(&attrs, arr);
+    if (attrs.type != cudaMemoryTypeDevice) {
+        return;
+    }
+
+    float* d_input_arr;
+    unsigned input_arr_len;
+    if (twoUpper == len) {
+        input_arr_len = len;
+        d_input_arr = arr;
+    } else {
+        // 需要 padding
+        input_arr_len = twoUpper;
+        cudaMalloc(&d_input_arr, sizeof(float) * input_arr_len);
+        // 然后初始化
+        cudaMemcpy(d_input_arr, arr, sizeof(float) * len, cudaMemcpyHostToDevice);
+        cudaMemset(d_input_arr + len, 0x7f, sizeof(float) * (input_arr_len - len));
+    }
+
+    dim3 grid_dim((input_arr_len / 256 == 0) ? 1 : input_arr_len / 256);
+    dim3 block_dim((input_arr_len / 256 == 0) ? input_arr_len : 256);
+
+    // 排序过程(重点)
+    for (unsigned stride = 2; stride <= input_arr_len; stride <<= 1) {
+        for (unsigned inner_stride = stride; inner_stride >= 2; inner_stride >>= 1) {
+            _bitonic_sort<<<grid_dim, block_dim>>>(d_input_arr, stride, inner_stride);
+        }
+    }
+
+    // 如果 padding 过，则此处还原
+    if (twoUpper != len) {
+        cudaMemcpy(arr, d_input_arr, sizeof(float) * len, cudaMemcpyDeviceToDevice);
+        cudaFree(d_input_arr);
+    }
+}
+
 int main(void) {
     int size = 100;
     float* values = (float*)malloc(size * sizeof(float));
+    float* dNums;
     srand(time(NULL));
     for (int i = 0; i < size; ++i) {
         values[i] = size - i;
     }
-
-    clock_t start = clock();
-    bitonic_sort(values, size);
+    CHECK(cudaMalloc((void**)&dNums, sizeof(float) * size));
+    CHECK(cudaMemcpy(dNums, values, sizeof(float) * size, cudaMemcpyHostToHost));
+    float_sort(dNums, size);
+    CHECK(cudaMemcpy(values,dNums,sizeof(float) * size, cudaMemcpyDeviceToHost));
     FILE* fp = NULL;
     fp = fopen("gz.txt", "w+");
     for (int i = 0; i < size; i++) {
-        fprintf(fp,"%d\t%f \n",i, values[i]);
+        fprintf(fp, "%d\t%f \n", i, values[i]);
     }
 }
 __global__ void bitonic_sort_step(float* dev_values, int j, int k) {
@@ -56,7 +134,7 @@ void bitonic_sort(float* values, int size) {
     cudaMalloc((void**)&dev_values, size * sizeof(float));
     cudaMemcpy(dev_values, values, size * sizeof(float), cudaMemcpyHostToDevice);
 
-    dim3 blocks(32, 1);                              /* Number of blocks   */
+    dim3 blocks(32, 1);                                /* Number of blocks   */
     dim3 threads((size + blocks.x - 1) / blocks.x, 1); /* Number of threads  */
 
     for (int k = 2; k <= size; k <<= 1) {
